2d9c9ab0345d0d15b531ab095388acde231a5e04
Slava <slavyanrts@mail.ru>
2019-02-19 12:01:12 +0400

    init files and db

--- helper.php
+++ helper.php
@@ -1,6 +1,7 @@
 <?php
 
 class ModSliderHelper{
+	
     public static function getSlider($params){
 
         $config = new JConfig();
@@ -11,10 +12,43 @@ class ModSliderHelper{
 
         if ($db->select($database_name)) {
 
-            
+            $query = $db->getQuery(true);
+            $query->select('*')
+                ->from('slides');
+
+            $db->setQuery($query);
+            $results = $db->loadObjectList();
+
+            $db->select($main_db);
+
+            return $results;
 
 
         }
 
     }
+	
+	public static function getOnly($params){
+		$config = new JConfig();
+        $main_db = $config->db;
+
+        $db =JFactory::getDBO();
+        $database_name = "sushi";
+
+        if ($db->select($database_name)) {
+
+            $query = $db->getQuery(true);
+            $query->select('*')
+                ->from('slides');
+
+            $db->setQuery($query);
+            $results = $db->loadObjectList();
+
+            $db->select($main_db);
+
+            return $results;
+
+
+        }
+	}
 }
--- tmpl/default.php
+++ tmpl/default.php
@@ -2,7 +2,13 @@
 // No direct access
 defined('_JEXEC') or die; ?>
 
-
+<?php
+                //$domain = idn_to_utf8($_SERVER['SERVER_NAME']);
+                //$subdomain = explode('.', $domain);
+                foreach($slides as $slide){
+					echo $slide->image . "<br>";
+                }
+                ?>
 
              
 
